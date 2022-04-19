111cf286158330c5d55b39314020243b63ffbb70
DESKTOP-TPQNTU5\Lenovo <knsol1992@Naver.com>
2022-04-19 23:34:43 +0900

    로그인 기능과 관련하여 새로운 테이블 4개 작성 (아크관계)

--- School/src/com/school/member/model/dao/School_DAO.java
+++ School/src/com/school/member/model/dao/School_DAO.java
@@ -1,4 +1,34 @@
 package com.school.member.model.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Map;
+import java.util.Properties;
+
 public class School_DAO {
+
+	private Properties prop = new Properties();
+	
+	public School_DAO() {
+		try {
+			prop.loadFromXML(new FileInputStream("mapper/query.xml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	public int login(Connection con, Map<String, String> login) {
+
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		String query = prop.getProperty("");
+		
+		return 0;
+	}
 }
--- School/src/com/school/member/model/service/School_Service.java
+++ School/src/com/school/member/model/service/School_Service.java
@@ -1,15 +1,25 @@
 package com.school.member.model.service;
 
+import java.sql.Connection;
 import java.util.Map;
 
 import com.school.member.model.dao.School_DAO;
+
+import static com.school.common.JDBCTemplate.getConnection;
+import static com.school.common.JDBCTemplate.close;
 
 public class School_Service {
 
 	private School_DAO sd = new School_DAO();
 
 	public int login(Map<String, String> login) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		Connection con = getConnection();
+		
+		int result = sd.login(con, login);
+		
+		close(con);
+		
+		return result;
 	}
 }
