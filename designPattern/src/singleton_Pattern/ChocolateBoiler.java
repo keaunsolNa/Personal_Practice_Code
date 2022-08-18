0f81eeb89deea9f47cd61b94b2f4e394386e7450
keaunsolNa <knsol1992@naver.com>
2022-08-18 19:02:25 +0900

    22.08.18 Singleton Pattern

---
+++ designPattern/src/singleton_Pattern/ChocolateBoiler.java
@@ -0,0 +1,40 @@
+package singleton_Pattern;
+
+public class ChocolateBoiler {
+	private boolean empty;
+	private boolean boiled;
+	
+	public ChocolateBoiler() {
+		empty = true;
+		boiled = false;
+	}
+	
+	public void fill() {
+		if (isEmpty()) {
+			empty = false;
+			boiled = false;
+		}
+	}
+	
+	public void drain() {
+		if(!isEmpty() && isBoiled()) {
+			
+			empty = true;
+		}
+	}
+	
+	public void boil() {
+		if(!isEmpty() && !isBoiled()) {
+			
+			boiled = true;
+		}
+	}
+	
+	public boolean isEmpty() {
+		return empty;
+	}
+	
+	public boolean isBoiled() {
+		return boiled;
+	}
+}
---
+++ designPattern/src/singleton_Pattern/ChocolateBoilerSingleton.java
@@ -0,0 +1,48 @@
+package singleton_Pattern;
+
+public class ChocolateBoilerSingleton {
+	private boolean empty;
+	private boolean boiled;
+
+	private static ChocolateBoilerSingleton uniqueInstance;
+	
+	private ChocolateBoilerSingleton() {
+		empty = true;
+		boiled = false;
+	}
+	
+	public static ChocolateBoilerSingleton getInstance() {
+		if(uniqueInstance == null) {
+			uniqueInstance = new ChocolateBoilerSingleton();
+		}
+		return uniqueInstance;
+	}
+	public void fill() {
+		if (isEmpty()) {
+			empty = false;
+			boiled = false;
+		}
+	}
+	
+	public void drain() {
+		if(!isEmpty() && isBoiled()) {
+			
+			empty = true;
+		}
+	}
+	
+	public void boil() {
+		if(!isEmpty() && !isBoiled()) {
+			
+			boiled = true;
+		}
+	}
+	
+	public boolean isEmpty() {
+		return empty;
+	}
+	
+	public boolean isBoiled() {
+		return boiled;
+	}
+}
