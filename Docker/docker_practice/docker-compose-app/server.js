706297ac0b4a1751a3273347941ceb64b3a1f22b
keaunsolNa <knsol1992@naver.com>
2023-03-21 22:58:16 +0900

    2023.03.21 Docker

--- Docker/docker_practice/docker-compose-app/server.js
+++ Docker/docker_practice/docker-compose-app/server.js
@@ -1,46 +1,24 @@
 const express = require("express");
 const redis = require("redis");
-
-// 레디스 client 생성
+//레디스 클라이언트 생성 
 const client = redis.createClient({
-
-    socket: {
-    
     host: "redis-server",
-    
     port: 6379
-    
-    }
-    
-});
+})
 
 const app = express();
 
-app.get('/', async (req, res) => {
+//숫자는 0 부터 시작합니다.
+client.set("number", 0);
 
-        await client.connect();
-
-        let number = await client.get('number');
-
-        if (number === null) {
-
-        number = 0;
-
-    }
-
-    console.log('Number: ' + number);
-
-    res.send("숫자가 1씩 올라갑니다. 숫자: " + number)
-
-    await client.set("number", parseInt(number) + 1)
-
-    await client.disconnect();;;
-
+app.get('/', (req, res) => {
+    client.get("number", (err, number) => {
+        //현재 숫자를 가져온 후에 1씩 올려줍니다.
+        res.send("숫자가 1씩 올라갑니다. 숫자: " + number)
+        client.set("number", parseInt(number) + 1)
+    })
 })
-    
-    
-    
-    
+
+
 app.listen(8080);
-    
 console.log('Server is running');
