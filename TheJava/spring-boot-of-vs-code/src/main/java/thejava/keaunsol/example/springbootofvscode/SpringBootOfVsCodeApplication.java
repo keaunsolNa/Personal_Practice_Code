327a89cd5aa9a85bd1faab72c7ac6b2cdadcf61e
KeaunsolNa <knsol1992@naver.com>
2023-05-02 20:52:07 +0900

    The Java 학습용 코드

---
+++ TheJava/spring-boot-of-vs-code/src/main/java/thejava/keaunsol/example/springbootofvscode/SpringBootOfVsCodeApplication.java
@@ -0,0 +1,13 @@
+package thejava.keaunsol.example.springbootofvscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOfVsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOfVsCodeApplication.class, args);
+	}
+
+}
---
+++ TheJava/spring-boot-of-vs-code/src/test/java/thejava/keaunsol/example/springbootofvscode/SpringBootOfVsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package thejava.keaunsol.example.springbootofvscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOfVsCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
